3fefb1a8ad44477b0e8d6d263f2fb0d983bbfb2b
Fatna AbdulTaib <phatnaabdul55@gmail.com>
2023-07-31 16:21:23 +0300

    done with the footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,32 @@
-function Footer(){
-    return(
-        <div>
-            <h1>
-                Abdul
-            </h1>
-        </div>
-    )
+import { BsFacebook } from "react-icons/bs";
+import { BsInstagram } from "react-icons/bs";
+import { BsYoutube } from "react-icons/bs";
 
+function Footer() {
+  return (
+    <div className=" bg-green-100">
+      <div className="flex justify-between container mx-auto p-2">
+        <h2 className="text-4xl font-bold text-green-500 ">
+          Sick <span className="text-black">2</span> Strong
+        </h2>
+        <div>
+          <h3 className="font-bold">Quick links</h3>
+          <ul>
+            <li className="hover:text-green-600">Home</li>
+            <li className="hover:text-green-600">AboutUs</li>
+            <li className="hover:text-green-600">Search</li>
+          </ul>
+        </div>
+        <div>
+          <h3 className="font-bold">Follow us</h3>
+          <div className="flex items-center gap-5">
+            <BsFacebook size={35} />
+            <BsInstagram size={35} />
+            <BsYoutube size={35} />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
-export default Footer
+export default Footer;
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,13 +1,20 @@
+import {AiOutlineShoppingCart} from "react-icons/ai"
 function Nav(){
     return(
-        <div>
-        <div>
-            <h2>Sick2Strong</h2>
-            <p>Home</p>
+        <div className="container mx-auto flex justify-between p-2">
+        <div className="flex gap-5 items-center">
+            <h2 className="text-4xl font-bold text-green-500 ">Sick <span className="text-black">2</span> Strong</h2>
+        
         </div>
-        <div>
-        <button>Login</button>
-        <button>Signup</button>
+        <div className="flex gap-5">
+        <p className="text-2xl hover:text-emerald-600	 cursor-pointer">Home</p>
+            <p className="text-2xl hover:text-emerald-600	">About Us </p>
+        </div>
+        <div className="flex gap-5 items-center">
+
+            <AiOutlineShoppingCart size={30}/>
+        <button className="text-2xl hover:text-emerald-600  border-2 rounded-full py-2 px-5 ">Login</button>
+        <button className="text-2xl hover:text-emerald-600 border-2 rounded-full py-2 px-5">Signup</button>
 
         </div>
         </div>
